06b04ad4d3a6be26e3de4981a05c1273ea04277a
LonelyFlame <maks.chursin@gmail.com>
2015-07-09 21:41:22 +0600

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : UnitController {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(Input.GetAxis("Horizontal"));
+		print(Input.GetAxis("Vertical"));
+	}
+}
---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class UnitController : MonoBehaviour {
+	protected Vector2 move = new Vector2(0, 0);
+	protected bool facingRight = true;
+	protected bool grounded = false;
+	
+	protected virtual void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+		
+	protected virtual void Move() {
+		GetComponent<Rigidbody2D>().velocity = move;
+		if (move.x > 0 && !facingRight) {
+			Flip();
+		} else if (move.x < 0 && facingRight) {
+			Flip();
+		}
+	}
+}
