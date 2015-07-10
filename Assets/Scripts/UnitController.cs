c2e7cdceb1f373464cdd71eae70e78c2a97e3e61
LonelyFlame <maks.chursin@gmail.com>
2015-07-10 10:19:50 +0600

    Updated moving units

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public abstract class UnitController : MonoBehaviour {
 	public int health = 100;
 	public float maxSpeed = 2f;
+	public float smoothTime = 0.1f;
 
 	protected Vector2 move = new Vector2(0, 0);
-	protected bool facingRight = true;
-	protected bool grounded = false;
+	protected bool facingRight = false;
+	protected Vector2 velocity;
 	
 	protected virtual void Flip() {
 		facingRight = !facingRight;
@@ -17,8 +18,23 @@ public abstract class UnitController : MonoBehaviour {
 	}
 		
 	protected virtual void Move() {
+		float targetX;
+		float targetY;
+		targetX = Mathf.SmoothDamp (
+			transform.position.x,
+			transform.position.x + move.x/5,
+			ref velocity.x,
+			smoothTime
+			);
+		targetY = Mathf.SmoothDamp (
+			transform.position.y,
+			transform.position.y + move.y/5,
+			ref velocity.y,
+			smoothTime
+		);
 //		GetComponent<Rigidbody2D>().velocity = new Vector2(move.x * maxSpeed, move.y * maxSpeed);
-		transform.position = new Vector3(transform.position.x + move.x/20, transform.position.y + move.y/20, transform.position.z);
+//		transform.position = new Vector3(transform.position.x + move.x/20, transform.position.y + move.y/20, transform.position.z);
+		transform.position = new Vector3(targetX, targetY, transform.position.z);
 		if (move.x > 0 && !facingRight) {
 			Flip();
 		} else if (move.x < 0 && facingRight) {
