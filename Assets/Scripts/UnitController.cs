581ba0424afd1b69aeabe3ce14fce22c3ac56273
LonelyFlame <maks.chursin@gmail.com>
2015-07-10 10:31:53 +0600

    Updated unit movings

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public abstract class UnitController : MonoBehaviour {
 	public int health = 100;
-	public float maxSpeed = 2f;
+	public float speed = 0.2f;
 	public float smoothTime = 0.1f;
 
 	protected Vector2 move = new Vector2(0, 0);
@@ -22,18 +22,16 @@ public abstract class UnitController : MonoBehaviour {
 		float targetY;
 		targetX = Mathf.SmoothDamp (
 			transform.position.x,
-			transform.position.x + move.x/5,
+			transform.position.x + move.x * speed,
 			ref velocity.x,
 			smoothTime
 			);
 		targetY = Mathf.SmoothDamp (
 			transform.position.y,
-			transform.position.y + move.y/5,
+			transform.position.y + move.y * speed,
 			ref velocity.y,
 			smoothTime
 		);
-//		GetComponent<Rigidbody2D>().velocity = new Vector2(move.x * maxSpeed, move.y * maxSpeed);
-//		transform.position = new Vector3(transform.position.x + move.x/20, transform.position.y + move.y/20, transform.position.z);
 		transform.position = new Vector3(targetX, targetY, transform.position.z);
 		if (move.x > 0 && !facingRight) {
 			Flip();
