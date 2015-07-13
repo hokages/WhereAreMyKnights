3900d982f17d644ccc16598e2f8374bb73f9d1ba
LonelyFlame <maks.chursin@gmail.com>
2015-07-13 22:17:06 +0600

    Added background script

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+	
+	public Transform target;
+	public Vector2 speed = new Vector2(0.5f, 0.5f);
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(target.position.x * speed.x, target.position.y * speed.y, transform.position.z);
+	}
+}
