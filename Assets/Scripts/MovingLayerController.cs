997a420025575b3b53d0a42becbd2f7c7572cbf4
LonelyFlame <maks.chursin@gmail.com>
2015-07-14 15:51:13 +0600

    Updated moving layer script

--- Assets/Scripts/MovingLayerController.cs
+++ Assets/Scripts/MovingLayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 
 public class MovingLayerController : MonoBehaviour {
-
+	
 	public MovingLayer[] layers;
 	
 	// Use this for initialization
@@ -31,12 +31,13 @@ public class MovingLayer {
 	[Tooltip("If set 0 will be used sprite width/height")]
 	public Vector2 size = new Vector2(0, 0);
 	public Vector2 speed = new Vector2(0, 0);
-
+	
 	private Vector3 movingSpeed3;
 	private List<MovingLayerPart> parts = new List<MovingLayerPart>();
+	private Vector2 sign = new Vector2(1, 1);
 	
 	public void Start() {
-
+		
 		if (size.x == 0) {
 			size.x = background.GetComponent<SpriteRenderer>().bounds.size.x;
 		}
@@ -49,43 +50,21 @@ public class MovingLayer {
 		mainSprite.setSize(size);
 		parts.Add(mainSprite);
 
+		sign.x = speed.x < 0 ? 1 : -1;
+		sign.y = speed.y < 0 ? 1 : -1;
+		
 		if (speed.y != 0) {
-			MovingLayerPart verticalSprite = new MovingLayerPart();
-			Transform verticalSpriteTransform = Transform.Instantiate(background) as Transform;
-			verticalSpriteTransform.transform.parent = background.transform.parent;
-			verticalSpriteTransform.transform.name = background.transform.name + " vertical clone";
-			verticalSprite.setSprite(verticalSpriteTransform);
-			int sign = speed.y < 0 ? 1 : -1;
-			verticalSprite.setDelta(new Vector2(0, size.y * sign));
-			verticalSprite.setSize(size);
-			parts.Add(verticalSprite);
+			setClone(0, size.y, "vertical");
 		}
 		
 		if (speed.x != 0) {
-			MovingLayerPart horisontalSprite = new MovingLayerPart();
-			Transform horisontalSpriteTransform = Transform.Instantiate(background) as Transform;
-			horisontalSpriteTransform.transform.parent = background.transform.parent;
-			horisontalSpriteTransform.transform.name = background.transform.name + " horisontal clone";
-			horisontalSprite.setSprite(horisontalSpriteTransform);
-			int sign = speed.x < 0 ? 1 : -1;
-			horisontalSprite.setDelta(new Vector2(size.x * sign, 0));
-			horisontalSprite.setSize(size);
-			parts.Add(horisontalSprite);
+			setClone(size.x, 0, "horisontal");
 		}
 		
 		if (speed.x != 0 && speed.y != 0) {
-			MovingLayerPart diagonalSprite = new MovingLayerPart();
-			Transform diagonalSpriteTransform = Transform.Instantiate(background) as Transform;
-			diagonalSpriteTransform.transform.parent = background.transform.parent;
-			diagonalSpriteTransform.transform.name = background.transform.name + " diagonal clone";
-			diagonalSprite.setSprite(diagonalSpriteTransform);
-			int signX = speed.x < 0 ? 1 : -1;
-			int signY = speed.y < 0 ? 1 : -1;
-			diagonalSprite.setDelta(new Vector2(size.x * signX, size.y * signY));
-			diagonalSprite.setSize(size);
-			parts.Add(diagonalSprite);
+			setClone(size.x, size.y, "diagonal");
 		}
-
+		
 		foreach (MovingLayerPart part in parts) {
 			part.Start();
 		}
@@ -97,6 +76,17 @@ public class MovingLayer {
 		}
 	}
 	
+	private void setClone(float sizeX = 0, float sizeY = 0, string name = "") {
+		MovingLayerPart verticalSprite = new MovingLayerPart();
+		Transform verticalSpriteTransform = Transform.Instantiate(background) as Transform;
+		verticalSpriteTransform.transform.parent = background.transform.parent;
+		verticalSpriteTransform.transform.name = background.transform.name + " " + name + " clone";
+		verticalSprite.setSprite(verticalSpriteTransform);
+		verticalSprite.setDelta(new Vector2(sizeX * sign.x, sizeY * sign.y));
+		verticalSprite.setSize(size);
+		parts.Add(verticalSprite);
+	}
+	
 }
 
 
@@ -105,9 +95,8 @@ public class MovingLayerPart {
 	private Transform transform;
 	private Vector2 size = new Vector2(0, 0);
 	private Vector2 delta = new Vector2(0, 0);
-	private Vector2 moveDelta = new Vector2(0, 0);
 	private Vector3 startPositin;
-
+	
 	public void setSprite(Transform newTransform) {
 		transform = newTransform;
 	}
@@ -117,13 +106,13 @@ public class MovingLayerPart {
 	public void setSize(Vector2 newSize) {
 		size = newSize;
 	}
-
+	
 	public void Start() {
 		transform.position = new Vector3(
 			transform.position.x + delta.x,
 			transform.position.y + delta.y,
 			transform.position.z
-		);
+			);
 		startPositin = transform.position;
 	}
 	public void Update(Vector2 speed) {
@@ -132,12 +121,19 @@ public class MovingLayerPart {
 			transform.position.y + speed.y,
 			transform.position.z
 		);
-		if (newPos.x - startPositin.x >= size.x) {
+
+		if (newPos.x - startPositin.x > size.x) {
 			newPos.x -= size.x;
+		} else if (newPos.x - startPositin.x < -size.x) {
+			newPos.x += size.x;
 		}
-		if (newPos.y - startPositin.y >= size.y) {
+
+		if (newPos.y - startPositin.y > size.y) {
 			newPos.y -= size.y;
+		} else if (newPos.y - startPositin.y < -size.y) {
+			newPos.y += size.y;
 		}
+
 		transform.position = newPos;
 	}
 }
