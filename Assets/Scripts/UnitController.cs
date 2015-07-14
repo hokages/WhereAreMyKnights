464f6eae945942dbc17888c758f4064606630025
LonelyFlame <maks.chursin@gmail.com>
2015-07-14 20:08:33 +0600

    Fixed unit script

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -11,7 +11,7 @@ public abstract class UnitController : MonoBehaviour {
 	protected Vector2 move = new Vector2(0, 0);
 	protected Vector2 velocity;
 	protected bool facingRight = false;
-	protected List<[string effect, float time]> effects = new List<string>();
+	protected List<[string effect: float time]> effects = new List<string>();
 	
 	protected virtual void Flip() {
 		facingRight = !facingRight;
