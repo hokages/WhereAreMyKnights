36a6738632508993ee3a60524c296d357b98a566
LonelyFlame <maks.chursin@gmail.com>
2015-07-14 19:36:21 +0600

    Added sword script

---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordController : WeaponController {
+	
+	protected override void CauseADamage(Transform target) {
+		target.GetComponent<EnemyController>().TakeADamage(damage);
+	}
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class WeaponController : MonoBehaviour {
+	public string hitboxTag;
+	public float damage = 10;
+	
+	public virtual void OnTriggerEnter2D(Collider2D hitBox) {
+		if (hitBox.tag == hitboxTag) {
+			Transform target = hitBox.GetComponent<Transform>().parent;
+			CauseADamage(target);
+			addEffect(target);
+		}
+	}
+
+	protected virtual void CauseADamage(Transform target){}
+
+	protected virtual void addEffect(Transform target){}
+}
