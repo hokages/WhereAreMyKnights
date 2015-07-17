b908e21b76ee89829fcb625e226ab3e7a9e4c6ed
LonelyFlame <maks.chursin@gmail.com>
2015-07-17 21:07:00 +0600

    Refactored hitbox script

---
+++ Assets/Scripts/HitboxController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitboxController : MonoBehaviour {
+
+	private string _unit = "player";
+	private UnitController _controller;
+
+	// Use this for initialization
+	void Start () {
+		_controller = transform.parent.GetComponent<UnitController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void provideDamage(float dmg) {
+		_controller.TakeADamage(dmg);
+				
+	}
+}
