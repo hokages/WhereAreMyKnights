76eaa2caa95bf26a6cc8b34328effedc21e1a6c3
LonelyFlame <maks.chursin@gmail.com>
2015-07-21 16:08:14 +0600

    Added sprites controller

---
+++ Assets/Scripts/SpriteController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteController : MonoBehaviour {
+	public Sprite[] sprites;
+
+	private float __sector;
+	private float __direction;
+	private UnitController __unitController;
+
+	// Use this for initialization
+	void Start () {
+		__unitController = transform.parent.GetComponent<UnitController>();
+		__sector = Mathf.PI * 2 / sprites.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newDirection = __unitController.getDirection();
+		if (newDirection != __direction) {
+			__direction = newDirection;
+			int sprite = (int) Mathf.Round(__direction / __sector);
+			if (sprite >= sprites.Length){
+				sprite = 0;
+			}
+			gameObject.GetComponent<SpriteRenderer>().sprite = sprites[sprite];
+		}
+	}
+}
